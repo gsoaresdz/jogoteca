23567dcdb8e0ba35e64f0d0d13bd63c0dc927f5e
Gabriel Soares <gsoaresdesouza@live.com>
2023-08-03 13:49:05 -0300

    Montando um formulário no Flask

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -6,21 +6,29 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
+jogo1 = Jogo('Tetris', 'Puzzle', 'PC')
+jogo2 = Jogo('God of War', 'Rock in Slash', 'PS2')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+
+lista = [jogo1, jogo2, jogo3]
+
 app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    jogo1 = Jogo('Tetris', 'Puzzle', 'PC')
-    jogo2 = Jogo('God of War', 'Rock in Slash', 'PS2')
-    jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
-   
-    lista = [jogo1, jogo2, jogo3]
-
     return render_template('lista.html', titulo = 'Jogos', jogos = lista)
-
 
 @app.route('/novo')
 def novo():
     return render_template('novo.html', titulo = 'Novo Jogo')
+
+@app.route('/criar')
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo = 'Jogos', jogos = lista)
 
 app.run()
