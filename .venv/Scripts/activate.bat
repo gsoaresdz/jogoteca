a65ef1869203aadb7469e14185f2d4e9ce30193c
Gabriel Soares <gsoaresdesouza@live.com>
2023-08-11 10:42:42 -0300

    Preparação do ambiente para conectar com o Banco de Dados MySQL

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\b2101015\OneDrive - BULLLA SA\ws-projetos-alura\jogoteca\.venv
+set VIRTUAL_ENV=C:\Users\Gabriel Soares\OneDrive - BULLLA IP S.A\ws-projetos-alura\jogoteca\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
